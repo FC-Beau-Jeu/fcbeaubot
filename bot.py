8f246744c24e2711fe157ee38f723e5c52d51c8f
Quentin Barbarat <quentin.barbarat@epita.fr>
2019-11-12 13:42:24 +0100

    Cleans up bot code

--- bot.py
+++ bot.py
@@ -3,6 +3,23 @@
 import twitter
 import settings
 
+
+def rt_tweet(api, tweet):
+    try:
+        api.PostRetweet(tweet.id)
+        print(f'[SUCCESS]: Retweeted - {tweet.text}')
+    except twitter.error.TwitterError:
+        print(f'[ERROR]: Could not retweet tweet - {tweet.id_str}')
+
+
+def like_tweet(api, tweet):
+    try:
+        api.CreateFavorite(tweet)
+        print(f'[SUCCESS]: Favorited - {tweet.text}')
+    except twitter.error.TwitterError:
+        print(f'[ERROR]: Could not favorite tweet - {tweet.id_str}')
+
+
 if __name__ == '__main__':
 
     api = twitter.Api(consumer_key=settings.consumer_key,
@@ -12,13 +29,8 @@ if __name__ == '__main__':
 
     api.VerifyCredentials()
 
-"""
-results = api.GetSearch(raw_query='q=%23billard&src=typeahead_click')
+    results = api.GetSearch(raw_query='q=%23billard&src=typeahead_click')
 
-for tweet in results:
-    try:
-        api.CreateFavorite(tweet)
-    except twitter.error.TwitterError:
-        print("Fav failed")
-    # api.PostRetweet(tweet)
-"""
+    for tweet in results:
+        like_tweet(api, tweet)
+        rt_tweet(api, tweet)
